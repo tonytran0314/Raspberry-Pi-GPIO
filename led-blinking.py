f28c744f226b7bbf3dcf2a403051eb005ad6337e
tonytran0314 <tonytran0314@gmail.com>
2021-08-14 17:32:20 -0400

    Edit: try Try Except feature to clear all data before end the program

--- led-blinking.py
+++ led-blinking.py
@@ -4,6 +4,7 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
+#set mode and switch off warnings
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
@@ -17,11 +18,24 @@ GPIO.setup(led, GPIO.OUT)
 # Turn led off first
 GPIO.output(led, False)
 
-# Loop that turn led on and off
-while True:
-    #led on for 1 sec
-    GPIO.output(led, True)
-    sleep(sleepTime)
-    #led off for 1 sec
-    GPIO.output(led, False)
-    sleep(sleepTime)
+try: 
+    while True:
+        #led on for 1 sec
+        GPIO.output(led, True)
+        sleep(sleepTime)
+        #led off for 1 sec
+        GPIO.output(led, False)
+        sleep(sleepTime)
+except KeyboardInterrupt:  
+    # here you put any code you want to run before the program   
+    # exits when you press CTRL+C  
+    print("Hello")
+  
+except:  
+    # this catches ALL other exceptions including errors.  
+    # You won't get any error messages for debugging  
+    # so only use it once your code is working  
+    print ("Other error or exception occurred!")  
+  
+finally:  
+    GPIO.cleanup() # this ensures a clean exit  
