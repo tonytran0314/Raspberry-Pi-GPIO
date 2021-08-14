7ba984e65aa1d0a307f5c0942b06d6bd72432071
tonytran0314 <tonytran0314@gmail.com>
2021-08-14 13:03:26 -0400

    Add: add code for led-blinking.py file

--- led-blinking.py
+++ led-blinking.py
@@ -1,1 +1,20 @@
 #led blinking
+
+import RPi.GPIO as GPIO
+import sleep from time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+led = 21
+sleepTime = 1
+
+GPIO.setup(led, GPIO.OUT)
+
+GPIO.output(led, False)
+
+while True:
+    GPIO.output(led, True)
+    sleep(sleepTime)
+    GPIO.output(led, False)
+    sleep(sleepTime)
