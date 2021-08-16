961a2c8f53684b34d25cda33a76179a70770ca8e
tonytran0314 <tonytran0314@gmail.com>
2021-08-16 16:08:23 -0400

    import time.sleep | delete initial declaration led status

--- button-led.py
+++ button-led.py
@@ -1,6 +1,7 @@
 # press button to turn the led on
 
 import RPi.GPIO as GPIO
+from time import sleep
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -10,7 +11,6 @@ ledPin = 21
 GPIO.setup(btnPin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(ledPin, GPIO.OUT)
 
-GPIO.output(ledPin, False)
 ledStatus = False
 btnVal = True
 
