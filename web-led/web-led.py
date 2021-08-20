5da2f220956ed878011a2a5a3312a4cf394f8e1a
tonytran0314 <tonytran0314@gmail.com>
2021-08-20 00:13:17 -0400

    add default route | turn on/off feature

--- web-led/web-led.py
+++ web-led/web-led.py
@@ -2,16 +2,31 @@
 
 #Server file
 
+import RPi.GPIO as GPIO
 from flask import Flask, render_template
+
+GPIO.setmode(GPIO.BCM)
+
+led = 21
+sleepTime = 1
+
+GPIO.setup(led, GPIO.OUT)
+GPIO.output(led, False)
 
 app = Flask(__name__)
 
+@app.route("/")
+def greeting():
+    return render_template("index.html")
+
 @app.route("/on")
 def on():
+    GPIO.output(led, True)
     return render_template("on.html") # must have templates folder to contain html files
 
 @app.route("/off")
 def off():
+    GPIO.output(led, False)
     return render_template("off.html")
 
 if __name__ == "__main__":
